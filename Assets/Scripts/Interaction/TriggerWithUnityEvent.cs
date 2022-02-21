4e75df5692ef7b9d8c3d2dcdcff0aea45faf2273
Aryo Damar Prasetia <aryo.damar01@gmail.com>
2022-02-21 17:39:12 +0700

    fix trigger enter event with photon

--- Assets/Scripts/Interaction/TriggerWithUnityEvent.cs
+++ Assets/Scripts/Interaction/TriggerWithUnityEvent.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
+using Photon.Pun;
 
 public class TriggerWithUnityEvent : MonoBehaviour {
 
@@ -10,16 +11,30 @@ public class TriggerWithUnityEvent : MonoBehaviour {
     [SerializeField, Space(10)] UnityEvent onTriggerEnter;
     [SerializeField] UnityEvent onTriggerExit;
 
+    [SerializeField] bool checkPhoton = false;
+
     private void OnTriggerEnter(Collider other) {
-        if (!other.gameObject.CompareTag(targetTag))
+        if (!other.gameObject.CompareTag(targetTag)) return;
+
+        if (!checkPhoton) {
+            onTriggerEnter.Invoke();
             return;
+        }
+
+        if (!other.gameObject.GetComponent<PhotonView>().IsMine) return;
 
         onTriggerEnter.Invoke();
     }
 
     private void OnTriggerExit(Collider other) {
-        if (!other.gameObject.CompareTag(targetTag))
+        if (!other.gameObject.CompareTag(targetTag)) return;
+
+        if (!checkPhoton) {
+            onTriggerExit.Invoke();
             return;
+        }
+
+        if (!other.gameObject.GetComponent<PhotonView>().IsMine) return;
 
         onTriggerExit.Invoke();
     }
